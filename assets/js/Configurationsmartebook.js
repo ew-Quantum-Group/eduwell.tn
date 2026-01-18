7d9aed2b6e970f10d7f68697f56e3e73990e9d78
Eduwell Quantum Group <108446261+ew-Quantum-Group@users.noreply.github.com>
2026-01-18 06:42:31 -0800

    Add files via upload

---
+++ assets/js/Configurationsmartebook.js
@@ -0,0 +1,9 @@
+  // Configuration
+        // Structure des utilisateurs
+        const users = {
+            "50986": { "name": "Ahmed", "page": "page1.html" },
+            "78906": { "name": "Sophie", "page": "page2.html" },
+            "admin": { "name": "Administrateur", "page": "admin.html" },
+            "guest": { "name": "Invité", "page": "welcome.html" },
+            "demo": { "name": "Démo", "page": "demo.html" }
+        };

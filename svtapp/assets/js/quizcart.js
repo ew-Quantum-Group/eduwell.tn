676be7f598ef5c70574a2572004915355d199e97
Eduwell Quantum Group <108446261+ew-Quantum-Group@users.noreply.github.com>
2026-01-21 11:01:40 -0800

    Add files via upload

---
+++ svtapp/assets/js/quizcart.js
@@ -0,0 +1,188 @@
+
+        // JSON data structure for Popular Courses (Science-themed)
+        const popularCoursesData = {
+            "reproduction-humaine": {
+                title: "Reproduction Humaine",
+                description: "",
+                options: [
+                    {
+                        title: "Chez l'Homme Pack(1)",
+                        description: "",
+                        icon: "fas fa-male",
+                        link: "./pages/seek1.html",
+                        color: "blue"
+                    },
+                    {
+                        title: "Chez la Femme Pack(1)",
+                        description: "",
+                        icon: "fas fa-female",
+                        link: "./pages/seek3.html",
+                        color: "green"
+                    },
+                    {
+                        title: "Chez l'Homme Pack(2)",
+                        description: "",
+                        icon: "fas fa-male",
+                        link: "./pages/seek2.html",
+                        color: "blue"
+                    },
+                    {
+                        title: "Chez la Femme Pack(2)",
+                        description: "",
+                        icon: "fas fa-female",
+                        link: "./pages/seek4.html",
+                        color: "green"
+                    },
+                    {
+                        title: "Procréation Pack(1)",
+                        description: "",
+                        icon: "fas fa-baby",
+                        link: "./pages/seek5.html",
+                        color: "purple"
+                    },
+                    {
+                        title: "Procréation Pack(2)",
+                        description: "",
+                        icon: "fas fa-baby",
+                        link: "./pages/seek6.html",
+                        color: "purple"
+                    }
+                ]
+            },
+            "evolution-biologique": {
+                title: "Évolution Biologique",
+                description: "",
+                options: [
+                    {
+                        title: "Arguments",
+                        description: "",
+                        icon: "fas fa-search",
+                        link: "./pages/seek7.html",
+                        color: "blue"
+                    },
+                    {
+                        title: "Mécanismes",
+                        description: "",
+                        icon: "fas fa-cogs",
+                        link: "./pages/seek8.html",
+                        color: "purple"
+                    }
+                ]
+            },
+            "genetique-humaine": {
+                title: "Génétique Humaine",
+                description: "",
+                options: [
+                    {
+                        title: "Génétique des diploïdes (P1)",
+                        description: "",
+                        icon: "fas fa-dna",
+                        link: "./pages/seek11.html",
+                        color: "green"
+                    },
+                    {
+                        title: "Génétique des diploïdes (P2)",
+                        description: "",
+                        icon: "fas fa-dna",
+                        link: "#",
+                        color: "blue"
+                    },
+                    {
+                        title: "Génétique Humaine (1)",
+                        description: "",
+                        icon: "fas fa-user-md",
+                        link: "#",
+                        color: "green"
+                    },
+                    {
+                        title: "Génétique Humaine (2)",
+                        description: "",
+                        icon: "fas fa-user-md",
+                        link: "#",
+                        color: "purple"
+                    }
+                ]
+            },
+            "neurophysiologie": {
+                title: "Neurophysiologie",
+                description: "",
+                options: [
+                    {
+                        title: "Tissu Nerveux",
+                        description: "",
+                        icon: "fas fa-brain",
+                        link: "#",
+                        color: "green"
+                    },
+                    {
+                        title: "Réflexe myotatique (1)",
+                        description: "",
+                        icon: "fas fa-running",
+                        link: "#",
+                        color: "blue"
+                    },
+                    {
+                        title: "Réflexe myotatique (2)",
+                        description: "",
+                        icon: "fas fa-running",
+                        link: "#",
+                        color: "green"
+                    },
+                    {
+                        title: "Fonctionnement du muscle squelettique",
+                        description: "",
+                        icon: "fas fa-dumbbell",
+                        link: "#",
+                        color: "blue"
+                    },
+                    {
+                        title: "Régulation de la pression artérielle",
+                        description: "",
+                        icon: "fas fa-heartbeat",
+                        link: "#",
+                        color: "green"
+                    },
+                    {
+                        title: "Hygiène du système nerveux",
+                        description: "",
+                        icon: "fas fa-shield-alt",
+                        link: "#",
+                        color: "purple"
+                    }
+                ]
+            },
+            "immunite": {
+                title: "Immunité",
+                description: "",
+                options: [
+                    {
+                        title: "Réponse immunitaire spécifique (1)",
+                        description: "",
+                        icon: "fas fa-shield-virus",
+                        link: "#",
+                        color: "green"
+                    },
+                    {
+                        title: "Réponse immunitaire spécifique (2)",
+                        description: "",
+                        icon: "fas fa-shield-virus",
+                        link: "#",
+                        color: "blue"
+                    },
+                    {
+                        title: "Allergies",
+                        description: "",
+                        icon: "fas fa-allergies",
+                        link: "#",
+                        color: "green"
+                    },
+                    {
+                        title: "SIDA",
+                        description: "",
+                        icon: "fas fa-virus",
+                        link: "#",
+                        color: "blue"
+                    }
+                ]
+            }
+        };

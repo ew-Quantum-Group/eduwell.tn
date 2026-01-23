a76d9260bcd51ce552f17828ac9604b259bbef68
Eduwell Quantum Group <108446261+ew-Quantum-Group@users.noreply.github.com>
2026-01-22 21:39:04 -0800

    Refactor modelsData JSON structure for clarity

--- svtapp/assets/js/flashcarts.js
+++ svtapp/assets/js/flashcarts.js
@@ -29,13 +29,12 @@
                 title: "Évolution des Espèces",
                 description: "Illustrations et schémas montrant les principales étapes de l'évolution biologique",
                 icon: "fas fa-frog",
-                images: [
-                    { src: "https://drive.google.com/uc?export=view&id=1_OJnEyrY70oyKebFF_ZZUHj2T-Nf6gpH", alt: "Cellule 1" },
-                    { src: "./flashcarts/7.webp", alt: "Cellule 2" },
-                    { src: "./flashcarts/8.webp", alt: "Cellule 3" },
-                    { src: "./flashcarts/9.webp", alt: "Cellule 4" },
-                    { src: "./flashcarts/10.webp", alt: "Cellule 5" }
-                ]
+               images: [
+  { src: "https://drive.google.com/thumbnail?id=1_OJnEyrY70oyKebFF_ZZUHj2T-Nf6gpH&sz=w1000",alt: "Cellule 1"},
+  
+  {  src: "https://drive.google.com/thumbnail?id=1NTjml5dBl6AC9_Xrtou-0nFULyjPQS3x&sz=w1000",alt: "Cellule 1" }
+]
+
             },
             "genetique": {
                 title: "Génétique Moléculaire",
@@ -54,11 +53,23 @@
                 description: " Schémas détaillés du cerveau, des nerfs et de leurs fonctions",
                 icon: "fas fa-brain",
                 images: [
-                    { src: "./flashcarts/6.webp", alt: "Cellule 1" },
-                    { src: "./flashcarts/7.webp", alt: "Cellule 2" },
-                    { src: "./flashcarts/8.webp", alt: "Cellule 3" },
-                    { src: "./flashcarts/9.webp", alt: "Cellule 4" },
-                    { src: "./flashcarts/10.webp", alt: "Cellule 5" }
+                    { src: "https://drive.google.com/thumbnail?id=1ek0W8ZWOqSe_GOKaKWAJlLl7EgKDqfF2&sz=w1000",alt: "Cellule 1"},
+                    { src: "https://drive.google.com/thumbnail?id=1LOStn1SBXNSCzk94vWHeRtVPhoCKg8iW&sz=w1000",alt: "Cellule 2"},
+                    { src: "https://drive.google.com/thumbnail?id=1Ha-WUQskzEOxHTcLs2UJ2qnyJ9KqbAx6&sz=w1000",alt: "Cellule 3"},
+                    { src: "https://drive.google.com/thumbnail?id=1zQGYlzSxNanMpPF0T--AUndgX7aCgoke&sz=w1000",alt: "Cellule 4"},
+                    { src: "https://drive.google.com/thumbnail?id=14JizLYbt2kwXyPS8pVY6bMMAhPtbOQOj&sz=w1000",alt: "Cellule 5"},
+                    { src: "https://drive.google.com/thumbnail?id=1so5gU6kPIHvLwn-Hu3UFT3MLeJ_n5cOq&sz=w1000",alt: "Cellule 6"},
+                    { src: "https://drive.google.com/thumbnail?id=1mAxe5olLsW2JUdfO3ZB1CSodBW6sIenh&sz=w1000",alt: "Cellule 7"},
+                    { src: "https://drive.google.com/thumbnail?id=1rdbpEervmKrPDHB73C7xwesLy8voE4pZ&sz=w1000",alt: "Cellule 8"},
+                    { src: "https://drive.google.com/thumbnail?id=1Q1XGFkN_xZPi4255doQG7Vs9iE7ba7bw&sz=w1000",alt: "Cellule 9"},
+                    { src: "https://drive.google.com/thumbnail?id=1fqvUV--ees-9qlcWoxP5nDJL7ltm86Bk&sz=w1000",alt: "Cellule 10"},
+                    { src: "https://drive.google.com/thumbnail?id=1-S_3m177TcJex0AWX4s-c9DeugX871P4/&sz=w1000",alt: "Cellule 11"},
+                    { src: "https://drive.google.com/thumbnail?id=1djZJkRN4RavVlGEK3P8WhIxRW5aOoLb-&sz=w1000",alt: "Cellule 12"},
+                    { src: "https://drive.google.com/thumbnail?id=1USWIA4GW9K7irjhQUT8RK8cbqJdvrwE_&sz=w1000",alt: "Cellule 13"},
+                    { src: "https://drive.google.com/thumbnail?id=1v2e_G9zBbJ3TA8oTZNoDLfI7Fh4eUcif&sz=w1000",alt: "Cellule 14"},
+                    { src: "https://drive.google.com/thumbnail?id=13dsGS7LzTdxgPGGPawQKzvI_lCtG3qwE&sz=w1000",alt: "Cellule 15"},
+                    { src: "https://drive.google.com/thumbnail?id=1TEPOlDN_Jn0BauWZA7BSY5zhVBCCMbvj&sz=w1000",alt: "Cellule 16"}
+                    
                 ]
             },
             "musqule": {

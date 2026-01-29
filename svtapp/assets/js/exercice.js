07ff41ac462ec3c190916962c438faed2afe2516
Eduwell Quantum Group <108446261+ew-Quantum-Group@users.noreply.github.com>
2026-01-28 23:44:35 -0800

    Update exercice.js

--- svtapp/assets/js/exercice.js
+++ svtapp/assets/js/exercice.js
@@ -5,48 +5,104 @@
                 id: "ex01",
                 title: "Devoir Contrôle 1 · SVT",
                 description: "Toutes les copies types du Devoir de Contrôle 1 en SVT, analysées et expliquées pour préparer le Bac efficacement",
-                exerciseCount: "4",
+                exerciseCount: "16",
                 progress: 0,
                 pdfs: [
-                    { title: "Devoir C1 · Copie 1", url: "https://drive.google.com/file/d/19Vv6TvcjLp4EEyDbtrBAbJ4ChA6iSf4h/preview" },
-                    { title: "Devoir C1 · Copie 2", url: "https://drive.google.com/file/d/1efk9JOUP76seNloNQdFwTb6_p8Qwxt8R/preview" },
-                    { title: "Devoir C1 · Copie 3", url: "https://drive.google.com/file/d/1G32y-WsfAo1CcNd1vAW18mIspsThd3NU/preview" }
+                    { title: "Devoir de contrôle n°1- 1 ", url: "https://drive.google.com/file/d/1kV3SgxTy3OUdunymHPButv0NL6cjqc-l/preview" },
+                    { title: "Correction n°1- 1", url: "https://drive.google.com/file/d/1u_g16KgIoI_91VZuzZsGwIZKoMc10R34/preview" },
+                    { title: "Devoir de contrôle n°1- 2", url: "https://drive.google.com/file/d/1EGDdWmC6bFgxaUPvBfmckTQjo708TC09/preview" },
+                    { title: "Correction n°1- 2", url: "https://drive.google.com/file/d/1ScPH5pqQzB3rmHdCVDh2qCI8bywHw4hn/preview" },
+                    { title: "Devoir de contrôle n°1- 3 ", url: "https://drive.google.com/file/d/1XXf14GysWZ2ZiV_rKOg1Sopy0f11CDf_/preview" },
+                    { title: "Correction n°1- 3", url: "https://drive.google.com/file/d/1K25mNwH7ehgKZMf6o-U-RxZY_u3EhbZ2/preview" },
+                    { title: "Devoir de contrôle n°1- 4", url: "https://drive.google.com/file/d/10VOFLTJwEscEYs3R1kl0FWKcw9N0yxyR/preview" },
+                    { title: "Correction n°1- 4", url: "https://drive.google.com/file/d/1bhx9Uj5uUqbX6Yn_o_iJ7Gc-Qn2Bugkq/preview" },
+                    { title: "Devoir de contrôle n°1- 5", url: "https://drive.google.com/file/d/11YNhR45ZjtZ5t_EyEhH5gIlWtgeGeLqX/preview" },
+                    { title: "Correction n°1- 5", url: "https://drive.google.com/file/d/1looWDr8DWC7cF8VdzlKXypMAFAvzh15w/preview" },
+                    { title: "Devoir de contrôle n°1- 6", url: "https://drive.google.com/file/d/19JvFK_FSSVABX0WdcD1Vbfp7H6k3V-x9/preview" },
+                    { title: "Correction n°1- 6", url: "https://drive.google.com/file/d/1vOYh3f2GSSdEUnOAVn19chjGA52jop4w/preview" },
+                    { title: "Devoir de contrôle n°1- 7", url: "https://drive.google.com/file/d/1L7fmwkz9uXdYYQkvYebqhAeR-eW1Edy-/preview" },
+                    { title: "Correction n°1- 7", url: "https://drive.google.com/file/d/1f4n-CRWd_LbIAwxQSuYzRlPPp0gOSoQs/preview" },
+                    { title: "Devoir de contrôle n°1- 8", url: "https://drive.google.com/file/d/1uCLrk0b2yT0pZQzX-BNJZV7pryQWLIyl/preview" },
+                    { title: "Correction n°1- 8", url: "https://drive.google.com/file/d/1sXUW8XQdBbZ3lpIXAKqeydramXjlGeql/preview" }
+                   
                 ]
             },
              "ex09": {
                 id: "ex03",
                 title: "Devoir de Synthèse 1 · SVT",
                 description: "Analyse complète des devoirs SVT – Copies modèles et corrections pour le Bac",
-                exerciseCount: "3",
+                exerciseCount: "14",
                 progress: 0,
                 pdfs: [
-                    { title: "Devoir S1 · Copie 1", url: "https://drive.google.com/file/d/1XTl8S7_aek_-KOBD2ow9RY2CBSs4cflK/preview" },
-                    { title: "Devoir S1 · Copie 2", url: "https://drive.google.com/file/d/1bm0ngOyVmSrCe3Cdqdv0C_AhFElU8Gjx/preview" },
-                    { title: "Devoir S1 · Copie 2", url: "https://drive.google.com/file/d/1vVqFBqRb0kgLLev8Sc43S9I3x6uPscp-/preview" }
+                    { title: "Devoir de Synthèse n°1- 1", url: "https://drive.google.com/file/d/1t0Fg7h1bTTmx2vj8iW-cRBab5wauHE8P/preview" },
+                    { title: "Correction n°1- 1", url: "https://drive.google.com/file/d/15r6etwFagbEtFvwhFZppCEaV2-vKasPO/preview" },
+                     { title: "Devoir de Synthèse n°1- 2", url: "https://drive.google.com/file/d/1ZpEUvFYNJ8O-afCEv21Lt9CRtyIjaiKJ/preview" },
+                    { title: "Correction n°1- 2", url: "https://drive.google.com/file/d/1BqrO14cebgI5cpxkjsLYAugEOtoEfnNK/preview" },
+                     { title: "Devoir de Synthèse n°1- 3", url: "https://drive.google.com/file/d/1hMZjtQ0crvaGS7LVueIT1HsspGq46QPD/preview" },
+                    { title: "Correction n°1- 3", url: "https://drive.google.com/file/d/1J_OF8qoq4PdT4rmH5tuIQDJDn4wepJzj/preview" },
+                     { title: "Devoir de Synthèse n°1- 4", url: "https://drive.google.com/file/d/1m0XzvDWEuFFq2BVcfVpZKUBN6AqlypoF/preview" },
+                    { title: "Correction n°1- 4", url: "https://drive.google.com/file/d/1cRv8K5Fq_pRXh7ijeOMc_Pwt45CWJN-G/preview" },
+                     { title: "Devoir de Synthèse n°1- 5", url: "https://drive.google.com/file/d/1MoVImSn3gvRsFAvtEs_bDW1-ald3ATEx/preview" },
+                    { title: "Correction n°1- 5", url: "https://drive.google.com/file/d/1fATQ8WHKwNUw6IHw8k8MxoObGIP3NEaK/preview" },
+                     { title: "Devoir de Synthèse n°1- 6", url: "https://drive.google.com/file/d/1iRAZ4lmOS70JryDX9VIwejfgpe5oGuVP/preview" },
+                    { title: "Correction n°1- 6", url: "https://drive.google.com/file/d/1TJniXtIfLfe5RjtUn1B9PeJjWYn1WwqQ/preview" },
+                     { title: "Devoir de Synthèse n°1- 7", url: "https://drive.google.com/file/d/1JCUspvbQTTZiZQvuId_gAk6fsL7HjIvx/preview" },
+                    { title: "Correction n°1- 7", url: "https://drive.google.com/file/d/1gcT9sKOnNs-5MCAHHYxfGpsY4Wu-5qjJ/preview" }
+                   
                 ]
             },
              "ex07": {
                 id: "ex04",
                 title: "Devoir Contrôle 2 · SVT",
                 description: "Toutes les copies types du Devoir de Contrôle 2 en SVT, analysées et expliquées pour préparer le Bac efficacement",
-                exerciseCount: "4",
+                exerciseCount: "22",
                 progress: 0,
                 pdfs: [
-                    { title: "Devoir C2 · Copie 1", url: "https://drive.google.com/file/d/19Vv6TvcjLp4EEyDbtrBAbJ4ChA6iSf4h/preview" },
-                    { title: "Devoir C2 · Copie 2", url: "https://drive.google.com/file/d/1efk9JOUP76seNloNQdFwTb6_p8Qwxt8R/preview" },
-                    { title: "Devoir C2 · Copie 3", url: "https://drive.google.com/file/d/1G32y-WsfAo1CcNd1vAW18mIspsThd3NU/preview" }
+                     { title: "Devoir de contrôle n°2- 1 ", url: "https://drive.google.com/file/d/1vP-dy1ixVT_oV0qwXh51OiIeHSB_Dxn8/preview" },
+                    { title: "Correction n°2- 1", url: "https://drive.google.com/file/d/1iwR8n3kaovAin6fox7e99PKlezq24NNN/preview" },
+                      { title: "Devoir de contrôle n°2- 2 ", url: "https://drive.google.com/file/d/1x2SzBoyv2JPj72YHR17Cv9E5VEPAbtIx/preview" },
+                    { title: "Correction n°2- 2", url: "https://drive.google.com/file/d/1o-LIrPnHMfpZjDGWCzTNVaiT_cR2rsCp/preview" },
+                      { title: "Devoir de contrôle n°2- 3 ", url: "https://drive.google.com/file/d/1zAEVxQDn4AztwlKk_TP1-gjXX1L9ibvu/preview" },
+                    { title: "Correction n°2- 3", url: "https://drive.google.com/file/d/1AKW-G3QciSFjeVSoV4XqOy-fnjyaf0FG/preview" },
+                      { title: "Devoir de contrôle n°2- 4 ", url: "https://drive.google.com/file/d/1uokaWh3POG2l7SX4p0uepwDgu4tbniK5/preview" },
+                    { title: "Correction n°2- 4", url: "https://drive.google.com/file/d/1Xed2Pbau9jU9IUbb5pRJUobEVvjgQtad/preview" },
+                      { title: "Devoir de contrôle n°2- 5 ", url: "https://drive.google.com/file/d/1cckv9f2CP3oB3k98Cl7X2R4R3bMYt8id/preview" },
+                    { title: "Correction n°2- 5", url: "https://drive.google.com/file/d/1bi8gZkF3dsGRkNpgJdmBeSFTl61vxz0G/preview" },
+                      { title: "Devoir de contrôle n°2- 6 ", url: "https://drive.google.com/file/d/1QR7zWhGJrp9ED5EdFxzk1FemKrM3pua4/preview" },
+                    { title: "Correction n°2- 6", url: "https://drive.google.com/file/d/1o-EURyFQ6hQwonnmF5TRosD19dEqmv9H/preview" },
+                      { title: "Devoir de contrôle n°2- 7 ", url: "https://drive.google.com/file/d/1bLYFO6g2nM1flSgA0lN20WW1g1bMFlRu/preview" },
+                    { title: "Correction n°2- 7", url: "https://drive.google.com/file/d/1rTMQK-S-RGGps4iJLxdl62ruR5GU_Jmc/preview" },
+                      { title: "Devoir de contrôle n°2- 8 ", url: "https://drive.google.com/file/d/1Z0Avao0rWBKV63OBNjejN8g8ij8yupcK/preview" },
+                    { title: "Correction n°2- 8", url: "https://drive.google.com/file/d/18vkwSwxvTZ_v3QGYE7r56wY2qRzGLEr_/preview" },
+                      { title: "Devoir de contrôle n°2- 9 ", url: "https://drive.google.com/file/d/1fcFrxebQIAVaXhnHE-IXV6sItVZM15lL/preview" },
+                    { title: "Correction n°2- 9", url: "https://drive.google.com/file/d/1L0ZXQvL11faCLKmhMiB_LFGj3_SEBnSu/preview" },
+                      { title: "Devoir de contrôle n°2- 10 ", url: "https://drive.google.com/file/d/1307_PtyYtjCUJxp2lqkWC2_XJ4mqAL3J/preview" },
+                    { title: "Correction n°2- 10", url: "https://drive.google.com/file/d/1Rek3xlV9CvR6DHCH1E6W2rZWrzxqL45K/preview" },
+                      { title: "Devoir de contrôle n°2- 11 ", url: "https://drive.google.com/file/d/1UEfkXjxUgiksEq9OJuPR3OkRrR2ai6Le/preview" },
+                    { title: "Correction n°2- 11", url: "https://drive.google.com/file/d/1IVjoZzvuCAcVa29Fle06hi3KRiFukMPP/preview" }
+                   
                 ]
             },
              "ex05": {
                 id: "ex05",
                 title: "Devoir de Synthèse 2 · SVT",
                 description: "Analyse complète des devoirs SVT – Copies modèles et corrections pour le Bac",
-                exerciseCount: "3",
+                exerciseCount: "12",
                 progress: 0,
                 pdfs: [
-                    { title: "Devoir S2 · Copie 1", url: "https://drive.google.com/file/d/1XTl8S7_aek_-KOBD2ow9RY2CBSs4cflK/preview" },
-                    { title: "Devoir S2 · Copie 2", url: "https://drive.google.com/file/d/1bm0ngOyVmSrCe3Cdqdv0C_AhFElU8Gjx/preview" },
-                    { title: "Devoir S2 · Copie 2", url: "https://drive.google.com/file/d/1vVqFBqRb0kgLLev8Sc43S9I3x6uPscp-/preview" }
+                     { title: "Devoir de Synthèse n°2- 1", url: "https://drive.google.com/file/d/1BppMoMAQusin8pBD8aZRlz21DLJyOkv_/preview" },
+                    { title: "Correction n°2- 1", url: "https://drive.google.com/file/d/1MgQY2IcxI-OVcFLEUeIYbLyqEb5PyTc3/preview" },
+                      { title: "Devoir de Synthèse n°2- 2", url: "https://drive.google.com/file/d/1ljEq_kYXoz_TFxtxGmAg3DDiv4bmpvB1/preview" },
+                    { title: "Correction n°2- 2", url: "https://drive.google.com/file/d/1KVwdGlXKGonSFx0FdD_CNhRcXeAWptug/preview" },
+                      { title: "Devoir de Synthèse n°2- 3", url: "https://drive.google.com/file/d/1g_ucSrhYrk7ty4s5XZraJhY0VMFncX3J/preview" },
+                    { title: "Correction n°2- 3", url: "https://drive.google.com/file/d/1DRvRB5PkupxlOd0fV-v4uYu-bb9dZIQb/preview" },
+                      { title: "Devoir de Synthèse n°2- 4", url: "https://drive.google.com/file/d/1hH9kCXULmB4t_tHhyvkXouU7sstAg6hr/preview" },
+                    { title: "Correction n°2- 4", url: "https://drive.google.com/file/d/1s881DCVLaMafNIYR7Y9gebcGlJHaheOi/preview" },
+                      { title: "Devoir de Synthèse n°2- 5", url: "https://drive.google.com/file/d/1jjOuGivmRo3oxYCq56QgKLUmGBJnulZw/preview" },
+                    { title: "Correction n°2- 5", url: "https://drive.google.com/file/d/1qFl_1auY2MgnzzDEWNQxnbuo1P4mnmHX/preview" },
+                      { title: "Devoir de Synthèse n°2- 6", url: "https://drive.google.com/file/d/17jTtksChAQ3HTb-4iVTSkM3rfZo8fKJ8/preview" },
+                    { title: "Correction n°2- 6", url: "https://drive.google.com/file/d/174EzYYUa3IhVhIpc7r1W0NaK822YE_ag/preview" }
+                   
                 ]
             }, "ex06": {
                 id: "ex06",

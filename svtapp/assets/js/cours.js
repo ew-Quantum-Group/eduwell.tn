c4b70a9bbeaff03ad70b78605dc6437aadfa1c31
Eduwell Quantum Group <108446261+ew-Quantum-Group@users.noreply.github.com>
2026-02-06 10:08:10 -0800

    Update cours.js

--- svtapp/assets/js/cours.js
+++ svtapp/assets/js/cours.js
@@ -20,15 +20,30 @@
                 description: "Étudier l'hérédité et la variation des caractères chez l'humain",
                 pdfUrl: "https://drive.google.com/file/d/1hze7Bz3wn3mDRyn1qYQNU92QOLoDTCMf/preview"
             },
-            "genetique-humaine4": {
-                title: "#",
-                description: "Étudier l'hérédité et la variation des caractères chez l'humain",
-                pdfUrl: "https://drive.google.com/file/d/1hze7Bz3wn3mDRyn1qYQNU92QOLoDTCMf/preview"
+            "evolution-des-especes": {
+                title: "Évolution des Espèces",
+                description: "Ce cours présente les mécanismes de transformation des espèces au cours du temps.",
+                pdfUrl: "https://drive.google.com/file/d/15IWz9XARnuB8NB3es7DJj1W2WR3YOebg/preview"
             },
-            "genetique-humaine5": { 
-                title: "#",
-                description: "Étudier l'hérédité et la variation des caractères chez l'humain",
-                pdfUrl: "https://drive.google.com/file/d/1hze7Bz3wn3mDRyn1qYQNU92QOLoDTCMf/preview"
+            "système-nerveux": { 
+                title: "Système Nerveux",
+                description: "Contrôle les activités du corps en recevant, traitant et transmettant les messages nerveux.",
+                pdfUrl: "https://drive.google.com/file/d/1-MfQN24ogZHwOGgNVqAQ7OwNGl1T46-I/preview"
+            },
+            "musqule": { 
+                title: "Système Musculo-Squelettique",
+                description: "Permet le mouvement du corps et assure le soutien grâce aux muscles et aux os.",
+                pdfUrl: "https://drive.google.com/file/d/1oK8wddtjMIzMr3ps8LNmzfGQrUC6ay-g/preview"
+            },
+            "presion": { 
+                title: "Pression Artérielle",
+                description: "Mesure la force du sang contre les parois des artères et reflète la santé du cœur et des vaisseaux.",
+                pdfUrl: "https://drive.google.com/file/d/1cXm2ILBGgjQK8xCRuLpIv8BX2UrH_fRh/preview"
+            },
+             "immu": { 
+                title: "Système Immunitaire",
+                description: "Protège le corps contre les infections en reconnaissant et en combattant les agents étrangers.",
+                pdfUrl: "https://drive.google.com/file/d/#/preview"
             }
         };
 

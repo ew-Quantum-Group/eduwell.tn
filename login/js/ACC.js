016896cf1df5f5f2d4629bf6cdadd7b691efd5f7
Eduwell Quantum Group <108446261+ew-Quantum-Group@users.noreply.github.com>
2026-02-09 21:01:51 -0800

    Update ACC.js

--- login/js/ACC.js
+++ login/js/ACC.js
@@ -1,23 +1,14 @@
     // CRITICAL: Must match index.html EXACTLY
         const VALID_CREDENTIALS = [
             { 
-                username: 'admin',
-                password: 'admin123', 
-                name: 'Administrator' 
+                username: 'aminesgatni53@gmail.com',
+                password: 'KoKo200@', 
+                name: 'ssuri' 
             },
+           
             { 
-                username: 'student',
-                password: 'student123', 
-                name: 'Student' 
-            },
-            { 
-                username: 'teacher',
-                password: 'teacher123', 
-                name: 'Teacher' 
-            },
-            { 
-                username: 'guest',
-                password: 'guest123', 
-                name: 'Guest' 
+                username: 'rawenmeddeb80@gmail.com',
+                password: 'Bacsciences2025@@', 
+                name: 'Rawen' 
             }
         ];

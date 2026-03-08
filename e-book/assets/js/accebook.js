c35d458f33d4bde0c6a60770acb58fa3dbcf49b8
Eduwell Quantum Group <108446261+ew-Quantum-Group@users.noreply.github.com>
2026-03-07 20:19:41 -0800

    Delete e-book directory

--- e-book/assets/js/accebook.js
+++
@@ -1,9 +0,0 @@
- // Authorized emails - ADD YOUR EMAILS HERE
-        const authorizedEmails = [
-            
-            "aminesgatni53@gmail.com",
-            "rawenmeddeb80@gmail.com",
-            "bensaidasma09@gmail.com",
-            "edubridgthijhg64@gmail.com"
-        ];
-        
